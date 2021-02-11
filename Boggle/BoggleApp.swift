7109be67d9d1106fcc96758d14151096a74c50ff
Oliver Foggin <oliverfoggin@me.com>
2021-02-11 17:24:12 +0000

    Initial Commit

---
+++ Boggle/BoggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoggleApp.swift
+//  Boggle
+//
+//  Created by Foggin, Oliver (Developer) on 11/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct BoggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
